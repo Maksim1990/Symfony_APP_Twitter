cb5b99f598c403d69b3a6022cec92e1129f826b5
maxim.narushevich <maxi@digitalmind.be>
2019-01-23 16:13:35 +0300

    - Added support of redirection when add new post - Fixed issue with debug profiler

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,38 +2,38 @@
 namespace App\Controller;
 
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * @Route("/blog")
  */
-class BaseController {
+class BaseController extends AbstractController{
 
-    private $twig;
     private $session;
-    public function __construct(\Twig_Environment $twig,SessionInterface $session)
+
+    public function __construct(SessionInterface $session)
     {
-        $this->twig=$twig;
+
         $this->session=$session;
     }
 
     /**
      * @Route("/",name="blog_index")
      *
-     * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
     public function index(){
         $posts=$this->session->get("posts");
-        dd($posts);
-        $html=$this->twig->render("base.html.twig",['message'=>"Test"]);
-       return new Response($html);
+
+        return $this->render("blog/index.html.twig",['posts'=>$posts]);
     }
 
     /**
@@ -47,7 +47,7 @@ class BaseController {
         ];
         $this->session->set("posts",$posts);
 
-        return new Response("Created");
+        return $this->redirectToRoute('blog_index');
     }
 
     /**
@@ -56,7 +56,10 @@ class BaseController {
     public function show($id){
         $posts=$this->session->get("posts");
         if(!empty($posts) && isset($posts[$id])){
-            dd($posts[$id]);
+            return $this->render("blog/show.html.twig",[
+                'id'=>$id,
+                'post'=>$posts[$id],
+            ]);
         }
         throw new NotFoundHttpException("Post not found!");
     }
